2a53dcf49b35c7411c47788a62e57e10147f5d75
Daniel <danstraw86@gmail.com>
2019-02-25 18:16:41 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = 3000;
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  })
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    name: 'Daniel',
+    pageTitle: 'Welcome',
+    welcomeMessage: 'Welcome to my website. Some of my interests are:',
+    likes: ['beer', 'coding', 'reading', 'gaming'],
+  })
+  // res.send({
+  //   name: 'Daniel',
+  //   likes: ['beer', 'coding', 'reading', 'gaming']
+  // });
+  // res.send('text');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About',
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send(
+    {
+      errorMessage: 'bad request',
+      errorCode: 404
+    }
+  );
+})
+app.listen(port, () => {
+  console.log(`server is listening at http://localhost:${port}`)
+});
+
