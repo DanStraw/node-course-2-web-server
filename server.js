0a08f21e12241c8925118363fa5ff753819a645b
Daniel <danstraw86@gmail.com>
2019-02-26 17:03:17 -0800

    added projects page

--- server.js
+++ server.js
@@ -56,6 +56,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects',
+    projects: ['SportsJunkies', 'RipeMetrics', 'Pick-A-Pet', 'DineTime']
+  })
+})
+
 app.get('/bad', (req, res) => {
   res.send(
     {
